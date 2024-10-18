30934b1bc55cc76330c52e84ef1c8ae83b995cfd
llykas03 <98225952+llykas03@users.noreply.github.com>
2024-10-18 14:19:21 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿namespace DuckShootingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inicijalizacija
+            Random random = new Random();
+            int totalDucks = 8;
+            int bullets = 20;
+            int ducksKilled = 0;
+            int[] duckPositions = new int[totalDucks];
+
+            // Dodeljivanje random pozicija patkama
+            for (int i = 0; i < totalDucks; i++)
+            {
+                duckPositions[i] = random.Next(1, 6);
+            }
+
+            // Početak igre
+            Console.WriteLine("Dobrodošli u lov na patke!");
+            Console.WriteLine("Imate 20 metaka. Unesite broj pozicije (1-5) da biste pucali.");
+
+            while (bullets > 0 && ducksKilled < totalDucks)
+            {
+                Console.WriteLine("\nPatke kvacu...");
+                for (int i = 0; i < totalDucks; i++)
+                {
+                    Console.Write("Quack ");
+                }
+
+                Console.Write("\nUnesite broj pozicije: ");
+                int playerGuess = int.Parse(Console.ReadLine());
+
+                bool hit = false;
+                for (int i = 0; i < totalDucks; i++)
+                {
+                    if (duckPositions[i] == playerGuess)
+                    {
+                        Console.WriteLine("Bang! Pogodili ste patku!");
+                        ducksKilled++;
+                        hit = true;
+                        break;
+                    }
+                }
+
+                if (!hit)
+                {
+                    Console.WriteLine("Promašili ste!");
+                }
+
+                bullets--;
+            }
+
+            // Kraj igre
+            if (ducksKilled == totalDucks)
+            {
+                Console.WriteLine("Čestitamo! Ubili ste sve patke!");
+            }
+            else
+            {
+                Console.WriteLine("Game over! Ponestalo vam je metaka.");
+            }
+        }
+    }
+}
